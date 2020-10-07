5bae1c5236ab2691abaaa181cb1a948d6ceed4fa
Jacrys3 <ryse0528@gmail.com>
2020-10-07 11:15:49 -0700

    Fixed StackOverflow Error but I want to make the try catch so I can see what tests are failing

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -7,19 +7,25 @@ public class Problem1 {
         // Add your solution
         int mid = data.length/2;
 
-        if(data == null || data.length == 0){
+        if(data.length == 0){
             return -1;
         }
-        else if(target == data[mid]){
-            return mid;
+        else if (data.length == 1 && data[0] == target){
+            return 0;
         }
-        else if (data[mid] > target){
+        else if(target == data[mid]){
+            return mid - 1;
+        }
+        else if (data.length > 1 && data[mid] > target){
             int[] copy = Arrays.copyOfRange(data, mid, data.length);
             return binarySearch(copy, target);
         }
-        else{
+        else if (data.length > 1 && data[mid] < target){
             int[] copy = Arrays.copyOfRange(data, 0, mid);
             return binarySearch(copy, target);
+        }
+        else {
+            return -1;
         }
 
     }
