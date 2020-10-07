60db1f48d6719b68de9634e5f5613595a25057da
Jacrys3 <ryse0528@gmail.com>
2020-10-07 11:08:54 -0700

    Returns StackOverflowError

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,8 +1,26 @@
+import java.util.Arrays;
+
 public class Problem1 {
 
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        return -1; // Place holder
+        int mid = data.length/2;
+
+        if(data == null || data.length == 0){
+            return -1;
+        }
+        else if(target == data[mid]){
+            return mid;
+        }
+        else if (data[mid] > target){
+            int[] copy = Arrays.copyOfRange(data, mid, data.length);
+            return binarySearch(copy, target);
+        }
+        else{
+            int[] copy = Arrays.copyOfRange(data, 0, mid);
+            return binarySearch(copy, target);
+        }
+
     }
 }
