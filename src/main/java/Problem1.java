6d6f6a758ee4747aec9c00d4e9c955fbc03f144e
Jacrys3 <ryse0528@gmail.com>
2020-10-07 11:40:39 -0700

    Final look, cleaned up the comments

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,32 +1,9 @@
-import java.util.Arrays;
 
 public class Problem1 {
 
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        /*int mid = data.length/2;
-
-        if(data.length == 0){
-            return -1;
-        }
-        else if (data.length == 1 && data[0] == target){
-            return 0;
-        }
-        else if(target == data[mid]){
-            return mid - 1;
-        }
-        else if (data.length > 1 && data[mid] > target){
-            int[] copy = Arrays.copyOfRange(data, mid, data.length);
-            return binarySearch(copy, target);
-        }
-        else if (data.length > 1 && data[mid] < target){
-            int[] copy = Arrays.copyOfRange(data, 0, mid);
-            return binarySearch(copy, target);
-        }
-        else {
-            return -1;
-        }*/
 
         int l = 0;
         int r = data.length - 1;
