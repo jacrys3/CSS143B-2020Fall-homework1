4781def2c48b588980b49fff70a45bf51a4ce7a1
Jacrys3 <ryse0528@gmail.com>
2020-10-07 11:35:56 -0700

    should work but fails many tests

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -5,7 +5,7 @@ public class Problem1 {
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
         // Add your solution
-        int mid = data.length/2;
+        /*int mid = data.length/2;
 
         if(data.length == 0){
             return -1;
@@ -26,7 +26,30 @@ public class Problem1 {
         }
         else {
             return -1;
+        }*/
+
+        int l = 0;
+        int r = data.length - 1;
+        int mid = (l + r) / 2;
+
+        while(mid > 1){
+            if(data.length == 0){
+                return -1;
+            }
+            else if (data[mid] == target){
+                return mid;
+            }
+            else if (data[mid] > target){
+                l = mid;
+                mid = (l + r) / 2;
+            }
+            else if (data[mid] < target){
+                r = mid;
+                mid = (l + r) / 2;
+            }
         }
+        return -1;
+
 
     }
 }
