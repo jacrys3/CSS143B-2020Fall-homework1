2459f297ff3454eb9fc0d82d82b749627b9009eb
Jacrys3 <ryse0528@gmail.com>
2020-10-07 16:49:38 -0700

    Sorts correctly but leaves out last number in array

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,7 +2,27 @@ public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
-        // Add your solution
+        // Arrays will be sorted low to high
+        int first;
+        int second;
+        if(data.length > 1) {
+            for (int k = 0; k < data.length - 1; k++) {
+                for (int i = 0; i < data.length - 1; i++) {
+                    first = data[i];
+                    second = data[i + 1];
+
+                    if (first > second) {
+                        data[i] = second;
+                        data[i + 1] = first;
+                    }
+                    System.out.println(data[i]);
+                }
+                System.out.println(" ");
+            }
+        }
+
+
+        //System.out.println(data[data.length - 1]);
 
     }
 }
--- src/test/java/Problem2Test.java
+++ src/test/java/Problem2Test.java
@@ -8,7 +8,39 @@ public class Problem2Test {
     // Okay to change
     @Test
     public void testBubbleSort() {
-        assertTrue(false);
-        // add your tests here
+        int inputs[][] = {
+                {1, 2, 3, 4, 5},
+                {4, 3, 5, 2, 1},
+                {},
+                {1},
+                {1, 1, 1},
+                {4, 4, 2, 1, 2},
+                {5, 4, 3, 2, 1},
+                {-1,3,-5},
+                {-2},
+                {-3,-2,-1,0}
+        };
+        int answers[][] = {
+                {1, 2, 3, 4, 5},
+                {1, 2, 3, 4, 5},
+                {},
+                {1},
+                {1, 1, 1},
+                {1, 2, 2, 4, 4},
+                {1, 2, 3, 4, 5},
+                {-5,-1,3},
+                {-2},
+                {-3,-2,-1,0}
+        };
+        assertEquals(inputs.length, answers.length);
+        for (int i = 0; i < inputs.length; i++) {
+            Problem2.bubbleSort(inputs[i]);
+            try {
+                assertEquals(inputs[i], answers[i]);
+            } catch(AssertionError e) {
+                System.out.println("");
+                System.out.println("**Test case " + i + " failed: " + e);
+            }
+        }
     }
 }
